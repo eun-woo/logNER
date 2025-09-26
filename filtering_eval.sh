784580c8c1bc0a97be648cc8b181e755d2c3924f
eun-woo <eunwoo5380@naver.como>
2025-09-26 11:26:59 +0000

    Add tradeoff files

--- filtering_eval.sh
+++ filtering_eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1000 1000 10000)
+for i in $(seq 1000 1000 50000)
 do
     echo "$i크기 필터링 수행"
     python logNER_parsing.py --log_file ./log_file/apache_iot.log --filtering_size $i
